2c6ee6d3627fd057a38d1c9be51fb6f41ece9fca
Francisca <francisca.orwa@student.moringaschool.com>
2022-11-21 13:28:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+ companyName;
+
+let mostProfitableNeighborhood ='Chelsea'
+ function mostProfitableNeighborhood(name){
+    return name;
+ }
+
+ let companyCeo ='Susan Smith'
+ function companyCeo(name){
+    return name;
+ }
